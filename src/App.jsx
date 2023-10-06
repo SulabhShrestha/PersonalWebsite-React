8b60735b66d94c351ec451b0e335727f172020d1
Sulabh Shrestha <Shresthasulabh777@gmail.com>
2023-10-06 20:02:42 +0545

    Skills added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Hero />
         <Projects />
+        <Skills />
       </main>
     </div>
   );
---
+++ src/components/Skills.jsx
@@ -0,0 +1,27 @@
+function Skills() {
+  return (
+    <section className="px-8">
+      <h2 className="text-4xl">What can I do?</h2>
+      <div className="flex gap-2 md:gap-16 justify-center">
+        <div className="website max-w-md">
+          <h3>Website</h3>
+          <p>
+            I'm proficient in MERN (MongoDB, Express.js, React, Node.js) web
+            development, creating dynamic and responsive websites for your
+            online presence.
+          </p>
+        </div>
+
+        <div className="mobile max-w-md">
+          <h3>Mobile App</h3>
+          <p>
+            I specialize in crafting Android apps, turning your ideas into
+            user-friendly, feature-rich applications for the Android platform.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
