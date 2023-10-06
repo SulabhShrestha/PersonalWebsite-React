37bb9213035410b42649b7cadd9fd7746d08e97f
Sulabh Shrestha <Shresthasulabh777@gmail.com>
2023-10-06 15:37:19 +0545

    Hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div className="bg-white h-[100vh]">
+    <div className="bg-white h-[100vh] ">
       <Header />
+      <main>
+        <Hero />
+      </main>
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+function Hero() {
+  return (
+    <section className="flex md:bg-green-400 md:flex-row flex-col items-center px-8">
+      <div className="me flex-[2]">
+        <h1 className="text-7xl">Hi, I'm Sulabh</h1>
+        <p>Web and Mobile app developer</p>
+      </div>
+
+      <div className="my-img flex-1">
+        <img
+          src="/src/assets/dog.jpg"
+          alt="My image"
+          className="rounded-[54%46%67%33%/54%40%60%46%]"
+          width="300"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
